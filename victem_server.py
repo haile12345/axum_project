6066365bbfac0b10a330caf4d3e6243cb0583ba8
haile anteneh <haile12345>
2025-12-14 09:38:03 -0300

    iii

--- victem_server.py
+++ victem_server.py
@@ -25,4 +25,4 @@ def admin_role():
 
 if __name__ == '__main__':
     print("Fake AWS Metadata: http://localhost:8080")
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081)
